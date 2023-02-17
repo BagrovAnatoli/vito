bb015c5e0bd9c6c1eed043d5de1e81af226f0ace
BagrovAnatoli <bagrov.anatoli@gmail.com>
2023-02-17 21:31:54 +0300

    show phone onClick

--- src/components/articleButton.jsx
+++ src/components/articleButton.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ClassesContext from '../pages/context';
 
 function ArticleButton({ phone }) {
     const classes = React.useContext(ClassesContext);
+    const [statusButton, setStatusButton] = useState('secret');
+
+    const clickHandler = (e) => {
+        if (statusButton === 'secret') {
+            alert(phone);
+            e.target.innerText = phone;
+        }
+        return setStatusButton('displayed');
+    };
 
     return (
-        <button className={`${classes.article__btn} ${classes['btn-hov02']}`}>Показать&nbsp;телефон
+        <button
+          className={`${classes.article__btn} ${classes['btn-hov02']}`}
+          onClick={clickHandler}
+        >Показать&nbsp;телефон
             <span>8&nbsp;{phone.slice(1, 4)}&nbsp;ХХХ&nbsp;ХХ&nbsp;ХХ</span>
         </button>
     );

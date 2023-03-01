c5686311f468341c88361d488bfd7bb2c508d013
BagrovAnatoli <bagrov.anatoli@gmail.com>
2023-03-01 12:50:11 +0300

    add input style

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -2,15 +2,16 @@
 import classes from './index.module.scss';
 import Logo from '../LogoText';
 import ButtonMain from '../ButtonMain';
+import Input from '../Input';
 
 const LoginModal = () => (
     // <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
     <form className={classes.form} >
       <Logo className={classes.logo} />
       <div className={classes.inputs}>
-        <input type="text" name="login" placeholder="логин" />
-        <input type="text" name="email" placeholder="email" />
-        <input type="password" name="password" placeholder="парроль" />
+        <Input type="text" name="login" placeholder="логин" />
+        <Input type="text" name="email" placeholder="email" />
+        <Input type="password" name="password" placeholder="парроль" />
       </div>
       <div className={classes.buttons}>
         <ButtonMain type="submit" content='Войти' />

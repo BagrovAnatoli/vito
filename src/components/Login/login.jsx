8c2db1b47a08449ffd7d1480c39cdf1ad3210b81
BagrovAnatoli <bagrov.anatoli@gmail.com>
2023-03-01 12:13:34 +0300

    modal login

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -1,6 +1,27 @@
 /* eslint-disable react/function-component-definition */
+import classes from './index.module.scss';
+import Logo from '../LogoText';
+import ButtonMain from '../ButtonMain';
+
 const LoginModal = () => (
-    <div>Login</div>
+    // <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
+    <form className={classes.form} >
+      <Logo className={classes.logo} />
+      <div className={classes.inputs}>
+        <input type="text" name="login" placeholder="логин" />
+        <input type="text" name="email" placeholder="email" />
+        <input type="password" name="password" placeholder="парроль" />
+      </div>
+      <div className={classes.buttons}>
+        <ButtonMain type="submit" content='Войти' />
+        <ButtonMain
+          content="Зарегистрироваться"
+          colorBtn={'white'}
+        //   onClick={showSignup}
+        />
+        {/* {error && <div className={classes.message}>{error}</div>} */}
+      </div>
+    </form>
 );
 
 export default LoginModal;
---
+++ src/components/LogoText/index.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+
+import LogoIcon from '../LogoIcon2';
+
+import cn from 'classnames';
+import classes from './index.module.css';
+
+const Logo = ({ colorLogo, className }) => {
+  const color = colorLogo === 'white' ? 'white' : 'black';
+
+  return (
+    <div className={classes.container}>
+      <Link to="/">
+        <LogoIcon className={cn(className, classes.icon, classes[color])} />
+      </Link>
+    </div>
+  );
+}
+
+export default Logo;

c979911070cc95fb131b3ea606a4039f9e12efbb
BagrovAnatoli <bagrov.anatoli@gmail.com>
2023-03-05 20:40:50 +0300

    add Fields

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,28 +1,43 @@
 /* eslint-disable react/function-component-definition */
+import { Form, Field } from 'react-final-form';
 import classes from './index.module.scss';
 import Logo from '../LogoText';
 import ButtonMain from '../ButtonMain';
 import Input from '../Input';
 
-const LoginModal = () => (
-    // <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
-    <form className={classes.form}>
-      <Logo className={classes.logo} />
-      <div className={classes.inputs}>
-        <Input type="text" name="login" placeholder="логин" />
-        <Input type="text" name="email" placeholder="email" />
-        <Input type="password" name="password" placeholder="парроль" />
-      </div>
-      <div className={classes.buttons}>
-        <ButtonMain type="submit" content="Войти" />
-        <ButtonMain
-          content="Зарегистрироваться"
-          colorBtn="white"
-        //   onClick={showSignup}
-        />
-        {/* {error && <div className={classes.message}>{error}</div>} */}
-      </div>
-    </form>
-);
+const LoginModal = () => {
+  const onSubmit = (values) => {
+    const value = values.search;
+    window.alert(JSON.stringify(value, 0, 2));
+  };
+  return (
+    <Form
+      onSubmit={onSubmit}
+      render={({ handleSubmit, values }) => (
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Logo className={classes.logo} />
+          <div className={classes.inputs}>
+            <Field name="login" component={Input} type="text" placeholder="логин" />
+            {/* <Input type="text" name="login" placeholder="логин" /> */}
+            <Field name="email" component={Input} type="text" placeholder="email" />
+            {/* <Input type="text" name="email" placeholder="email" /> */}
+            <Field name="password" component={Input} type="password" placeholder="пароль" />
+            {/* <Input type="password" name="password" placeholder="пароль" /> */}
+          </div>
+          <div className={classes.buttons}>
+            <ButtonMain type="submit" content="Войти" />
+            <ButtonMain
+              content="Зарегистрироваться"
+              colorBtn="white"
+            //   onClick={showSignup}
+            />
+            <pre>{JSON.stringify(values, 0, 2)}</pre>
+            {/* {error && <div className={classes.message}>{error}</div>} */}
+          </div>
+        </form>
+      )}
+    />
+  );
+};
 
 export default LoginModal;

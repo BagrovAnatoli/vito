ab1eb92ab3a8ab748c9e1a2462c60bcc9e8658df
BagrovAnatoli <bagrov.anatoli@gmail.com>
2023-03-06 20:47:39 +0300

    add fields validation

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,15 +1,20 @@
+/* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/function-component-definition */
 import { Form, Field } from 'react-final-form';
 import classes from './index.module.scss';
 import Logo from '../LogoText';
 import ButtonMain from '../ButtonMain';
-import Input from '../Input';
 
 const LoginModal = () => {
   const onSubmit = (values) => {
-    const value = values.search;
-    window.alert(JSON.stringify(value, 0, 2));
+    window.alert(JSON.stringify(values, 0, 2));
   };
+
+  const required = (value) => (value ? undefined : 'Обязательное поле');
+  const minLength = (length) => (value) => (value?.length >= length ? undefined : 'Минимум 3 символа');
+  const mustBeEmail = (value) => (value?.includes('@') ? undefined : 'Не похоже на email');
+  const composeValidators = (...validators) => (value) => validators.reduce((error, validator) => error || validator(value), undefined);
+
   return (
     <Form
       onSubmit={onSubmit}
@@ -17,12 +22,33 @@ const LoginModal = () => {
         <form className={classes.form} onSubmit={handleSubmit}>
           <Logo className={classes.logo} />
           <div className={classes.inputs}>
-            <Field name="login" component={Input} type="text" placeholder="логин" />
-            {/* <Input type="text" name="login" placeholder="логин" /> */}
-            <Field name="email" component={Input} type="text" placeholder="email" />
-            {/* <Input type="text" name="email" placeholder="email" /> */}
-            <Field name="password" component={Input} type="password" placeholder="пароль" />
-            {/* <Input type="password" name="password" placeholder="пароль" /> */}
+            {/* <Field name="login" component="input" type="text" placeholder="логин" className={classes.input} />
+            <Field name="email" component="input" type="text" placeholder="email" className={classes.input} />
+            <Field name="password" component="input" type="password" placeholder="пароль" className={classes.input} /> */}
+            <Field name="login" validate={composeValidators(required, minLength(3))}>
+              {({ input, meta }) => (
+                <>
+                  <input {...input} type="text" placeholder="логин" className={classes.input} />
+                  {meta.error && meta.touched && <span className={classes.message}>{meta.error}</span>}
+                </>
+              )}
+            </Field>
+            <Field name="email" validate={composeValidators(required, minLength(3), mustBeEmail)}>
+              {({ input, meta }) => (
+                <>
+                  <input {...input} type="text" placeholder="email" className={classes.input} />
+                  {meta.error && meta.touched && <span className={classes.message}>{meta.error}</span>}
+                </>
+              )}
+            </Field>
+            <Field name="password" validate={composeValidators(required, minLength(3))}>
+              {({ input, meta }) => (
+                <>
+                  <input {...input} type="password" placeholder="пароль" className={classes.input} />
+                  {meta.error && meta.touched && <span className={classes.message}>{meta.error}</span>}
+                </>
+              )}
+            </Field>
           </div>
           <div className={classes.buttons}>
             <ButtonMain type="submit" content="Войти" />

069a6e1c77be75078ad5026cdc33559b760927c9
BagrovAnatoli <bagrov.anatoli@gmail.com>
2023-03-14 16:46:45 +0300

    save

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,5 +1,8 @@
-import React from 'react';
-import { ClassesContext } from '../../contexts';
+import React, { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { ClassesContext, AuthContext } from '../../contexts';
+import { logout } from '../../store/actions/thunks/auth';
 import Header from '../../components/header';
 import Footer from '../../components/footer';
 import Logo from '../../components/logo';
@@ -8,20 +11,75 @@ import Button from '../../components/button';
 import HeaderButton from '../../components/headerButton';
 import Card from '../../components/card';
 import classes from './index.module.scss';
+import Modal from '../../components/Modal';
+import Login from '../../components/Login/index';
+import Register from '../../components/Register/index';
 
 function ProfilePage() {
+    const [modalVisible, setModalVisible] = useState(false);
+    const [modalContent, setModalContent] = useState('login');
+    const { isAuth } = useContext(AuthContext);
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const createAdHandler = () => {
+        alert("Создать объявление");
+    }
+
+    const registerHandler = () => {
+        setModalContent('register');
+    };
+
+    const loginHandler = () => {
+        setModalContent('login');
+        setModalVisible(true);
+    };
+
+    const successLoginHandler = () => {
+        setModalVisible(false);
+    };
+
+    const successRegisterHandler = () => {
+        setModalContent('login');
+    };
+
+    const logoutHandler = () => {
+        dispatch(logout());
+    };
+
+    const toggleModal = () => {
+        setModalVisible(!modalVisible);
+    };
+
     return (
         <ClassesContext.Provider value={classes}>
             <div className={classes.wrapper}>
                 <div className={classes.container}>
                     <Header>
                         <HeaderLogo />
-                        <HeaderButton className={classes['header__btn-putAd']} id="btputAd">
+                        {!isAuth
+                        ? (
+                            <HeaderButton className={classes['header__btn-main-enter']} id="btnMainEnter" onClick={loginHandler}>
+                                Вход в личный кабинет
+                            </HeaderButton>
+                        )
+                        : (
+                            <>
+                                <HeaderButton className={classes['header__btn-putAd']} id="btputAd" onClick={createAdHandler}>
+                                    Разместить объявление
+                                </HeaderButton>
+                                <HeaderButton className={classes['header__btn-main-out']} id="btnMainOut" onClick={logoutHandler}>
+                                    Выйти
+                                </HeaderButton>
+                            </>
+                        )}
+                        {/* <HeaderButton className={classes['header__btn-putAd']} id="btputAd">
                             Разместить объявление
                         </HeaderButton>
                         <HeaderButton className={classes['header__btn-lk']} id="btnlk">
                             Личный кабинет
-                        </HeaderButton>
+                        </HeaderButton> */}
                     </Header>
                     <main className={classes.main}>
                         <div className={classes.main__container}>
@@ -32,49 +90,61 @@ function ProfilePage() {
                                         <Button id="btnGoBack">Вернуться на&nbsp;главную</Button>
                                     </form>
                                 </div>
-                                <h2 className={classes.main__h2}>Здравствуйте, Антон!</h2>
-                                <div className={`${classes.main__profile} ${classes.profile}`}>
-                                    <div className={classes.profile__content}>
-                                        <h3 className={`${classes.profile__title} ${classes.title}`}>Настройки профиля</h3>
-                                        <div className={`${classes.profile__settings} ${classes.settings}`}>
-                                            <div className={classes.settings__left}>
-                                                <div className={classes.settings__img}>
-                                                    <a href="/" target="_self">
-                                                        <img src="#" alt="" />
+                                <h2 className={classes.main__h2}>{ isAuth ? "Здравствуйте, Антон!"
+                                : "Здравствуйте, гость!" }</h2>
+                                {isAuth
+                                ? (
+                                    <div className={`${classes.main__profile} ${classes.profile}`}>
+                                        <div className={classes.profile__content}>
+                                            <h3 className={`${classes.profile__title} ${classes.title}`}>Настройки профиля</h3>
+                                            <div className={`${classes.profile__settings} ${classes.settings}`}>
+                                                <div className={classes.settings__left}>
+                                                    <div className={classes.settings__img}>
+                                                        <a href="/" target="_self">
+                                                            <img src="#" alt="" />
+                                                        </a>
+                                                    </div>
+                                                    <a className={classes['settings__change-photo']} href="/" target="_self">
+                                                        Заменить
                                                     </a>
                                                 </div>
-                                                <a className={classes['settings__change-photo']} href="/" target="_self">
-                                                    Заменить
-                                                </a>
-                                            </div>
-                                            <div className={classes.settings__right}>
-                                                <form className={classes.settings__form} action="#">
-                                                    <div className={classes.settings__div}>
-                                                        <label htmlFor="fname">Имя</label>
-                                                        <input className={classes['settings__f-name']} id="settings-fname" name="fname" type="text" value="Ан" placeholder="" />
-                                                    </div>
+                                                <div className={classes.settings__right}>
+                                                    <form className={classes.settings__form} action="#">
+                                                        <div className={classes.settings__div}>
+                                                            <label htmlFor="fname">Имя</label>
+                                                            <input className={classes['settings__f-name']} id="settings-fname" name="fname" type="text" value="Ан" placeholder="" />
+                                                        </div>
 
-                                                    <div className={classes.settings__div}>
-                                                        <label htmlFor="lname">Фамилия</label>
-                                                        <input className={classes['settings__l-name']} id="settings-lname" name="lname" type="text" value="Городецкий" placeholder="" />
-                                                    </div>
+                                                        <div className={classes.settings__div}>
+                                                            <label htmlFor="lname">Фамилия</label>
+                                                            <input className={classes['settings__l-name']} id="settings-lname" name="lname" type="text" value="Городецкий" placeholder="" />
+                                                        </div>
 
-                                                    <div className={classes.settings__div}>
-                                                        <label htmlFor="city">Город</label>
-                                                        <input className={classes.settings__city} id="settings-city" name="city" type="text" value="Санкт-Петербург" placeholder="" />
-                                                    </div>
+                                                        <div className={classes.settings__div}>
+                                                            <label htmlFor="city">Город</label>
+                                                            <input className={classes.settings__city} id="settings-city" name="city" type="text" value="Санкт-Петербург" placeholder="" />
+                                                        </div>
 
-                                                    <div className={classes.settings__div}>
-                                                        <label htmlFor="phone">Телефон</label>
-                                                        <input className={classes.settings__phone} id="settings-phone" name="phone" type="tel" value="89161234567" placeholder="+79161234567" />
-                                                    </div>
+                                                        <div className={classes.settings__div}>
+                                                            <label htmlFor="phone">Телефон</label>
+                                                            <input className={classes.settings__phone} id="settings-phone" name="phone" type="tel" value="89161234567" placeholder="+79161234567" />
+                                                        </div>
 
-                                                    <button className={`${classes.settings__btn} ${classes['btn-hov02']}`} id="settings-btn">Сохранить</button>
-                                                </form>
+                                                        <button className={`${classes.settings__btn} ${classes['btn-hov02']}`} id="settings-btn">Сохранить</button>
+                                                    </form>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
+                                )
+                                : (
+                                    <div className={`${classes.main__profile} ${classes.profile}`}>
+                                        <div className={classes.profile__content}>
+                                            Войдите или зарегистрируйтесь, чтобы пользоваться всеми возможностями сервиса
+                                        </div>
+                                    </div>
+                                )}
+                                
                                 <h3 className={`${classes.main__title} ${classes.title}`}>
                                     Мои товары
                                 </h3>
@@ -96,6 +166,13 @@ function ProfilePage() {
                     <Footer />
                 </div>
             </div>
+            {modalVisible
+            && (
+                <Modal onClick={toggleModal}>
+                    {modalContent === 'login' && <Login registerHandler={registerHandler} successLoginHandler={successLoginHandler} />}
+                    {modalContent === 'register' && <Register successRegisterHandler={successRegisterHandler} />}
+                </Modal>
+            )}
         </ClassesContext.Provider>
     );
 }

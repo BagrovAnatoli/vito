71fa344245a754ed8c14d146ed036e17aacaf421
BagrovAnatoli <bagrov.anatoli@gmail.com>
2023-03-27 21:39:15 +0300

    login logout on article page

--- src/pages/article/index.jsx
+++ src/pages/article/index.jsx
@@ -1,26 +1,89 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 // import { useParams } from 'react-router-dom';
-import { ClassesContext } from '../../contexts';
+import { ClassesContext, AuthContext } from '../../contexts';
+import { logout } from '../../store/actions/thunks/auth';
 import Header from '../../components/header';
 import Article from '../../components/article';
 import Footer from '../../components/footer';
 import HeaderLogo from '../../components/headerLogo';
 import HeaderButton from '../../components/headerButton';
 import classes from './index.module.scss';
+import Modal from '../../components/Modal';
+import Login from '../../components/Login/index';
+import Register from '../../components/Register/index';
 
 function ArticlePage() {
+    const [modalVisible, setModalVisible] = useState(true);
+    const [modalContent, setModalContent] = useState('login');
+    const { isAuth, logoutContext } = useContext(AuthContext);
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const registerHandler = () => {
+        setModalContent('register');
+    };
+
+    const loginHandler = () => {
+        setModalContent('login');
+        setModalVisible(true);
+    };
+
+    const putAdHandler = () => {
+        alert('putAd');
+    };
+
+    const successLoginHandler = () => {
+        setModalVisible(false);
+    };
+
+    const successRegisterHandler = () => {
+        setModalContent('login');
+    };
+
+    const logoutHandler = async () => {
+        await dispatch(logout());
+        logoutContext();
+    };
+
+    const enterHandler = () => navigate('/profile');
+
+    const toggleModal = () => {
+        setModalVisible(!modalVisible);
+    };
+
     return (
         <ClassesContext.Provider value={classes}>
             <div className={classes.wrapper}>
                 <div className={classes.container}>
                     <Header>
                         <HeaderLogo />
-                        <HeaderButton className={classes['header__btn-putAd']} id="btputAd">
-                            Разместить объявление
-                        </HeaderButton>
-                        <HeaderButton className={classes['header__btn-lk']} id="btnlk">
-                            Личный кабинет
-                        </HeaderButton>
+                        {!isAuth
+                        ? (
+                            <>
+                                <HeaderButton className={classes['header__btn-putAd']} id="btputAd" onClick={loginHandler}>
+                                    Разместить объявление
+                                </HeaderButton>
+                                <HeaderButton className={classes['header__btn-lk']} id="btnlk" onClick={loginHandler}>
+                                    Личный кабинет
+                                </HeaderButton>
+                            </>
+                        )
+                        : (
+                            <>
+                                <HeaderButton className={classes['header__btn-putAd']} id="btputAd" onClick={putAdHandler}>
+                                    Разместить объявление
+                                </HeaderButton>
+                                <HeaderButton className={classes['header__btn-main-enter']} id="btnMainEnter" onClick={enterHandler}>
+                                    Личный кабинет
+                                </HeaderButton>
+                                <HeaderButton className={classes['header__btn-main-out']} id="btnMainOut" onClick={logoutHandler}>
+                                    Выйти
+                                </HeaderButton>
+                            </>
+                        )}
                     </Header>
                     <main className={classes.main}>
                         <div className={classes.main__container}>
@@ -38,6 +101,13 @@ function ArticlePage() {
                     <Footer />
                 </div>
             </div>
+            {modalVisible
+            && (
+                <Modal onClick={toggleModal}>
+                    {modalContent === 'login' && <Login registerHandler={registerHandler} successLoginHandler={successLoginHandler} />}
+                    {modalContent === 'register' && <Register successRegisterHandler={successRegisterHandler} />}
+                </Modal>
+            )}
         </ClassesContext.Provider>
     );
 }
